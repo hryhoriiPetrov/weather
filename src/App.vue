a320494383f85bcca891df7faf4f33235ec0e373
Григорий <hryhorii@MacBook-Air-Grigorij.local>
2025-05-06 18:20:40 +0300

    init weather project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Button from './components/Button.vue';
+const id = 'blue';
+</script>
+
+<template>
+  <header class="button">лого</header>
+  <Button>
+    <template #icon>
+      +
+    </template>
+    Сохранить
+  </Button>
+  <main :id>контент</main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <button class="button">
+    <div>
+      <slot name="icon" />
+    </div>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  border: none;
+  border-radius: 10px;
+  background: var(--gradient);
+  padding: 14px;
+  font-family: var(--font-montserrat);
+  font-size: 20px;
+  font-weight: 600;
+  color: var(--color-primary);
+  cursor: pointer;
+}
+.button:hover {
+  background: var(--gradient-inverted);
+}
+</style>
